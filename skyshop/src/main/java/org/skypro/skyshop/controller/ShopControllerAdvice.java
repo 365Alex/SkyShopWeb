2d6c67e52858bf6f5f46667f20bce9b8d22df636
master <aleshenko_ira_2017@mail.ru>
2025-09-22 09:39:00 +0300

    exception1

--- skyshop/src/main/java/org/skypro/skyshop/controller/ShopControllerAdvice.java
+++ skyshop/src/main/java/org/skypro/skyshop/controller/ShopControllerAdvice.java
@@ -6,11 +6,12 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class ShopControllerAdvice {
     @ExceptionHandler(NoSuchProductException.class)
-    public ResponseEntity<ShopError> noSuchProductException(NoSuchProductException s){
+    public ResponseEntity<ShopError> noSuchProductException(){
         ShopError shopError = new ShopError("NO_SUCH_PRODUCT_WITH_THIS_ID", "Продукт с таким id не найден");
         return new ResponseEntity<>(shopError, HttpStatusCode.valueOf(404));
     }
