925b23576f7005c06b3fa713355d2a48fdc4ae0c
master <aleshenko_ira_2017@mail.ru>
2025-09-29 11:32:15 +0300

    test

--- skyshop/src/test/java/org/skypro/skyshop/service/SearchServiceTest.java
+++ skyshop/src/test/java/org/skypro/skyshop/service/SearchServiceTest.java
@@ -1,8 +1,10 @@
 package org.skypro.skyshop.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.skypro.skyshop.search.SearchResult;
 
 import java.util.Collection;
@@ -12,7 +14,7 @@ import static com.fasterxml.jackson.databind.type.LogicalType.Collection;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
-
+@ExtendWith(MockitoExtension.class)
 public class SearchServiceTest {
     @Mock
     private StorageService storageServiceOne;
